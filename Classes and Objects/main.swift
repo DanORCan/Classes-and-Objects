d7be7ee616dabe717985763c961c37b2c8a28baf
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-12-14 18:45:26 -0500

    Initial Commit

---
+++ Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by DANIEL OREILLY on 12/14/18.
+//  Copyright © 2018 DANIEL OREILLY. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
